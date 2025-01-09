f560b46d8feede496b9d386201a37495f2b7bb31
suhasthegame <44828963+suhasthegame@users.noreply.github.com>
2025-01-09 16:58:15 -0500

    Updated config.py
    This file contains all the required ENV and config values in a central location

--- itseg/config.py
+++ itseg/config.py
@@ -3,3 +3,16 @@ ARTERIES_ANNOTATION_NAME = "arteries/arterioles"
 DEFAULT_ANNOTATIONS_REQ = [INTIMA_ANNOTATION_NAME, ARTERIES_ANNOTATION_NAME]
 DEFAULT_DOCKER_TMP_PATH = '/mnt/girder_worker'
 TMPDIR_ENV_NAME = 'TMPDIR'
+ARTERIES_FEATURE_FILE = 'arteriesarterioles_Features.xlsx'
+BBOX_INFO_FILE = 'box_coordinates.csv'
+COLLECTION_NAME = 'IntemaSegmenation'
+OP_FOLDER_NAME = 'outputs'
+PREDICTIONS_FOLDER_NAME = 'predictions'
+IMAGES_FOLDER_NAME = 'images'
+RESULT_XLSX_FILENAME = 'intimal_pipeline_results.xlsx'
+INTIMA_XLSX_COLNAMES = ['Sr No', 'Slide Name', 'Cropped Artery Name','Cropped Artery Index','Xmin','Ymin','Xmax','Ymax','CompartmentID', 'Luminal Area(pix2)', 'Luminal Radius(pix)', 'Intimal Area(pix2)', 'Average Intimal Thickness(pix)', 'Outer Boundary Intimal Thickness(pix)', 'isMaskComplete', 'Stenosis Ratio(%)']
+
+JSON_ANNOTATION_TEMPLATE = {
+        "elements": [],
+        "name": "Intema_Seg_output"
+}
