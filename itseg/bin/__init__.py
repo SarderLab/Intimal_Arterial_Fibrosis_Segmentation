49767ddd9c467670892d1837fca2b4922af9f547
suhasthegame <44828963+suhasthegame@users.noreply.github.com>
2025-01-09 20:24:25 -0500

    Added cli json file

--- itseg/bin/__init__.py
+++ itseg/bin/__init__.py
@@ -2,11 +2,12 @@ from .artery_cropping import *
 from .get_prediction import *
 from .evaluate_csv import *
 from .utils import *
+from .download_files import *
 
 
 
 __all__ = []
-for submodule in [artery_cropping, get_prediction, evaluate_csv, utils]:
+for submodule in [artery_cropping, get_prediction, evaluate_csv, utils,download_files]:
     try:
         __all__.extend(submodule.__all__)
     except AttributeError:
